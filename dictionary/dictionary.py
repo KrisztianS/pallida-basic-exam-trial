8be03ce7673ff08a475878aebc3ce973c8e69be7
Krisztian Szucs <krisztians0818@gmail.com>
2017-09-19 15:58:52 +0200

    feat(dictionary): initial commit, add to dictionary works

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -5,11 +5,12 @@ dictionary = [
 
 # Implement this method. It should add the given key-value pair to the
 # list 'dictionary'
-
+hun_word = "tojas"
+eng_word = "egg"
 
 def add_word(hun_word, eng_word):
-    pass
-
+    dictionary.append({hun_word: eng_word})
+    print(dictionary)
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
 
@@ -20,3 +21,5 @@ def translate_to_hun(eng_word):
 
 def translate_to_eng(hun_word):
     pass
+
+add_word(hun_word, eng_word)
